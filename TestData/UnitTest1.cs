39d2e5d5fcb2c316aa4a917e16762ad4eab104b4
Mikołaj Kubik <mikolaj.k.kubik@gmail.com>
2024-03-22 19:31:27 +0100

    Changed project structure.

---
+++ TestData/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace TestData;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ TestLogic/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace TestLogic;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}

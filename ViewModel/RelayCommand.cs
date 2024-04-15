bbf29ab1192c046e5b4ca6499eff698695b24682
KamilDzierzawski <kamildzierzawski@gmail.com>
2024-04-15 03:09:02 +0200

    Starter binding implementation

---
+++ ViewModel/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        public event EventHandler? CanExecuteChanged;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute;
+        }
+        
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+        
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+    }
+}
---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Xml.Resolvers;
+
+namespace ViewModel;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private String _numberOfBalls;
+    public ICommand _start { get; }
+    public ICommand _stop { get; }
+    private bool _isStartEnable;
+    private bool _isStopEnable;
+    private bool _isTextFieldEnable;
+
+    public ViewModel()
+    {
+        _numberOfBalls = "";
+        _start = new RelayCommand(Start);
+        _stop = new RelayCommand(Stop);
+
+        IsStartEnable = true;
+        IsStopEnable = false;
+        IsTextFieldEnable = true;
+    }
+
+    public void Start()
+    {
+        IsStartEnable = false;
+        IsStopEnable = true;
+        IsTextFieldEnable = false;
+    }
+
+    public void Stop()
+    {
+        IsStartEnable = true;
+        IsStopEnable = false;
+        IsTextFieldEnable = true;
+    }
+    
+    public string NumberOfBalls
+    {
+        get => _numberOfBalls;
+        set
+        {
+            _numberOfBalls = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsStartEnable
+    {
+        get => _isStartEnable;
+        set
+        {
+            _isStartEnable = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    public bool IsStopEnable
+    {
+        get => _isStopEnable;
+        set
+        {
+            _isStopEnable = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    public bool IsTextFieldEnable
+    {
+        get => _isTextFieldEnable;
+        set
+        {
+            _isTextFieldEnable = value;
+            OnPropertyChanged();
+        }
+    }
+}
